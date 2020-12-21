4635d309dd9c9952a82120870f013e094ed1bfc1
Lasse M <lasse.mollerup@typeform.com>
2020-12-21 23:24:33 +0100

    cleaned up the code in index.js and added comments to explain the code

--- src/server/index.js
+++ src/server/index.js
@@ -18,11 +18,14 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // Get information about Rover
 app.get('/rovers', async (req, res) => {
     
+    // determinating the URL depending on the state in the req
     const URLdependingOnDate = (req) => {
         
+        // changing the rover name to lower case according to the URL call
         const nameParam  = Object.assign(req.query.name.toLowerCase())
     
-
+        // Checking if imageDate has been changed and if the RoverChosen is not curiosity. 
+        // If so, a call is made for a specifc date wher both rovver Spirit and Opportunity has more images included.
         if((req.query.date !== '') && (nameParam != 'curiosity')){
        
             if(nameParam !== 'curiosity') {
@@ -30,9 +33,10 @@ app.get('/rovers', async (req, res) => {
             } 
            
         }
+        // The default call to get the latest photos for each of the three rovers 
         else return `https://api.nasa.gov/mars-photos/api/v1/rovers/${nameParam}/latest_photos?api_key=${process.env.API_KEY}`
     }
-
+    
     try {
         let data = await fetch(URLdependingOnDate(req))
           .then(res => 
